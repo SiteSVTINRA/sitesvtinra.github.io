9d0fc07d10a2c90b6c9c0b410aab56d6acd38fe6
sitesvtinra <57226541+SiteSVTINRA@users.noreply.github.com>
2019-11-10 15:51:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function NewMenu(text, href){
+	var nav = document.querySelector("nav ul");
+	nav.length=0;
+
+	var newMenu = document.createElement("li");
+	let link = document.createElement("a");
+	nav.appendChild(newMenu);
+	newMenu.appendChild(link);
+
+	link.innerHTML = text;
+	link.setAttribute("href", href);
+
+}
+
+NewMenu("Accueil", "index.html");
+NewMenu("L'INRA et le site de Gotheron", "hist.html");
+NewMenu("L'essai de pêchers visité", "essai.html");
+NewMenu("Bilan de notre sortie", "concl.html");
